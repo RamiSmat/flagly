0b58a7628bc7c89ae8d7958ca49e3e4616061877
rami <rami.smat@vonoy.co>
2026-07-15 16:27:26 +0100

    Merge GitHub repository with Spring Boot project

---
+++ src/test/java/flagly_api/FlaglyApiApplicationTests.java
@@ -0,0 +1,13 @@
+package flagly_api;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class FlaglyApiApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
